1085438cd92fdeceb21af8f1faf1700ef8a0cb7d
刘烁 <liushuo@tute.edu.cn>
2023-06-27 09:50:33 +0800

    fix_web_demo_aways_repeat_first_question_bug

--- web_demo.py
+++ web_demo.py
@@ -36,7 +36,7 @@ if __name__ == "__main__":
 
         with gr.Row():
             with gr.Column(scale=4):
-                txt = gr.Textbox(show_label=False, placeholder="Enter text and press enter", lines=11).style(
+                txt = gr.Textbox(show_label=False, placeholder="Enter text and press shift + enter", lines=11).style(
                     container=False)
             with gr.Column(scale=1):
                 max_length = gr.Slider(0, 4096, value=2048, step=1.0, label="Maximum length", interactive=True)
@@ -44,4 +44,5 @@ if __name__ == "__main__":
                 temperature = gr.Slider(0, 1, value=0.95, step=0.01, label="Temperature", interactive=True)
                 button = gr.Button("Generate")
         button.click(predict, [txt, state], [state] + text_boxes)
+        txt.submit(predict, [txt, state], [state] + text_boxes)
     demo.queue().launch(share=False, inbrowser=False, server_port=51234, server_name="0.0.0.0")
